7f3cdab77f3302c7914ce040ab5a7730f3d277ee
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-25 11:51:19 -0400

    Refactoring

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,34 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import static org.junit.Assert.*;
 public class StudentTest {
+    Student wes = new Student("Wesley", 123);
+    @Before
+    public void init(){
+        wes.addGrade(17);
+        wes.addGrade(97);
+    }
+
 
     @Test
    public void noGrades(){
-        Student wes = new Student("Wesley", 123);
-        assertTrue(wes.getGrades().isEmpty());
+        assertFalse(wes.getGrades().isEmpty());
     }
     @Test
     public void getIdTest(){
-        Student wes = new Student("Wesley", 123);
         assertEquals(123, wes.getId());
     }
     @Test
     public void getNameTest(){
-        Student wes = new Student("Wesley", 123);
         assertTrue(wes.getName().equalsIgnoreCase("Wesley"));
     }
     @Test
     public void addGradeTest(){
-        Student wes = new Student("Wesley", 123);
-        wes.addGrade(98);
         assertFalse(wes.getGrades().isEmpty());
     }
     @Test
     public void getGradesTest(){
-        Student wes = new Student("Wesley", 123);
-        wes.addGrade(17);
-        wes.addGrade(97);
         ArrayList<Integer> test = new ArrayList<>();
         test.add(17);
         test.add(97);
@@ -36,9 +36,6 @@ public class StudentTest {
     }
     @Test
     public void getAverageTest(){
-        Student wes = new Student("Wesley", 123);
-        wes.addGrade(17);
-        wes.addGrade(97);
         assertEquals(57, wes.getGradeAverage(), 0);
     }
 }
